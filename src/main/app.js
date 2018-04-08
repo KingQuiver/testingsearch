477ade5adb828db3679f63f349e5044baa4b3060
KingHeron <django@heronfoundation.org>
2018-04-08 06:15:50 +0100

    commit workin 1st time

--- src/main/app.js
+++ src/main/app.js
@@ -39,7 +39,7 @@ app.on('ready', () => {
     height: 365
   });
 
-  mainWindow.loadURL(`file:///Users/equinox/Documents/GitHub/DropSearch/src/frontend`)
+  mainWindow.loadURL(`file://${__dirname}/../frontend/index.html`)
 });
 
 ipcMain.on('github-oauth', (event, arg) => {
